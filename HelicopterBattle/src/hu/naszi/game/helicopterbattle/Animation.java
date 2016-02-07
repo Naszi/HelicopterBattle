90ea85d5ddb171fb0b0fccff66cefec8ce44277f
naszi <naszi75@gmail.com>
2016-02-07 21:01:50 +0100

    Version 4

--- HelicopterBattle/src/hu/naszi/game/helicopterbattle/Animation.java
+++ HelicopterBattle/src/hu/naszi/game/helicopterbattle/Animation.java
@@ -1,5 +1,83 @@
 package hu.naszi.game.helicopterbattle;
 
-public class Animation {
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
 
+public class Animation {
+	private BufferedImage animImage;
+	private int frameWidth;
+	private int frameHeight;
+	private int numberOfFrames;
+	private long frameTime;
+	private long startingFrameTime;
+	private long timeForNextFrame;
+	private long currentFrameNumber;
+	private boolean loop;
+
+	public int x;
+	public int y;
+
+	private int startingXOfFrameInImage;
+	private int endingXOfFrameInImage;
+
+	public boolean active;
+
+	private long showDelay;
+
+	private long timeOfAnimationCration;
+
+	public Animation(BufferedImage animImage, int frameWidth, int frameHeight, int numberOfFrames, long frameTime,
+			boolean loop, int x, int y, long showDelay) {
+		this.animImage = animImage;
+		this.frameWidth = frameWidth;
+		this.frameHeight = frameHeight;
+		this.numberOfFrames = numberOfFrames;
+		this.frameTime = frameTime;
+		this.loop = loop;
+
+		this.x = x;
+		this.y = y;
+
+		this.showDelay = showDelay;
+
+		timeOfAnimationCration = System.currentTimeMillis();
+
+		startingXOfFrameInImage = 0;
+		endingXOfFrameInImage = frameWidth;
+
+		startingFrameTime = System.currentTimeMillis() + showDelay;
+		timeForNextFrame = startingFrameTime + this.frameTime;
+		currentFrameNumber = 0;
+		active = true;
+	}
+
+	public void changeCoordinates(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	private void Update() {
+		if (timeForNextFrame <= System.currentTimeMillis()) {
+			currentFrameNumber++;
+			if (currentFrameNumber >= numberOfFrames) {
+				currentFrameNumber = 0;
+				if (!loop) {
+					active = false;
+				}
+			}
+			startingXOfFrameInImage = (int) (currentFrameNumber * frameWidth);
+			endingXOfFrameInImage = (int) (startingFrameTime * frameTime);
+
+			startingFrameTime = System.currentTimeMillis();
+			timeForNextFrame = startingFrameTime + frameTime;
+		}
+	}
+
+	public void Draw(Graphics2D g2d) {
+		this.Update();
+		if (this.timeOfAnimationCration + this.showDelay <= System.currentTimeMillis()) {
+			g2d.drawImage(animImage, x, x, x + frameWidth, y + frameHeight, startingXOfFrameInImage, 0,
+					endingXOfFrameInImage, frameHeight, null);
+		}
+	}
 }
